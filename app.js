d16cac6f76fafbd968a577f6b18954a19fd3ba4f
madhi-naga <madhi.naga@gmail.com>
2021-03-28 19:10:58 -0400

    Connected to mongodb

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const cors = require("cors");
-
+const MongoClient = require('mongodb').MongoClient
 const app = express();
 
+require('dotenv').config();
+const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ftlbl.mongodb.net/leaderboard?retryWrites=true&w=majority`;
+
 var corsOptions = {
-  origin: "http://localhost:6000"
+    origin: "http://localhost:8080"
 };
+
+MongoClient.connect(connectionString, {useUnifiedTopology: true}, (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+})
 
 app.use(cors(corsOptions));
 
 // simple route
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to the Brainblast Backend.</h1>");
+    res.send("<h1>Welcome to the Brainblast Backend</h1>");
+});
+
+app.get("/leaderboardtotal", (req, res) => {
+    res.send("<h1>Welcome to the Brainblast Backend</h1>");
 });
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
